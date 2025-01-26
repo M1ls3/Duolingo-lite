cbad1cb7f359cdc6aa55573ccfdf6c4c2c7a6038
M1ls3 <sofarua@gmail.com>
2025-01-26 22:45:21 +0200

    Refactor all fragments and add item list layout

--- app/src/main/java/com/example/duolingolite/ui/database/DatabaseFragment.kt
+++ app/src/main/java/com/example/duolingolite/ui/database/DatabaseFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.duolingolite.databinding.FragmentGalleryBinding
+import com.example.duolingolite.databinding.FragmentDatabaseBinding
 
 class DatabaseFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentDatabaseBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,13 +25,13 @@ class DatabaseFragment : Fragment() {
         val galleryViewModel =
             ViewModelProvider(this).get(DatabaseViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentDatabaseBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textGallery
+//        galleryViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
