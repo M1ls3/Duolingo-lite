a6b2da584252405065aac93e1b6cb2157276bd58
M1ls3 <sofarua@gmail.com>
2025-01-26 20:21:03 +0200

    v0.1

--- app/src/main/java/com/example/duolingolite/ui/database/DatabaseViewModel.kt
+++ app/src/main/java/com/example/duolingolite/ui/database/DatabaseViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.duolingolite.ui.gallery
+package com.example.duolingolite.ui.database
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class DatabaseViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is DB Fragment"
     }
     val text: LiveData<String> = _text
 }
